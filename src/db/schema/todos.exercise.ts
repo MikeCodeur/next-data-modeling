b34c100c38b4cee9d69e5804e0cbabe8e8065c32
Mike Codeur <contact@mikecodeur.com>
2024-08-21 14:45:02 +0800

    exo

--- src/db/schema/todos.exercise.ts
+++ src/db/schema/todos.exercise.ts
@@ -1,11 +1,18 @@
-import {date, pgTable, serial, varchar, boolean} from 'drizzle-orm/pg-core'
+// 🐶 Modelise 'Todo' grace à Drizzle
+import {pgTable, serial, varchar, boolean, timestamp} from 'drizzle-orm/pg-core'
 
 export const todos = pgTable('todo', {
   id: serial('id').primaryKey(),
-  title: varchar('title', {length: 256}),
-  isCompleted: boolean('iscompleted'),
-  updatedAt: date('updatedat'),
-  createdAt: date('createdat'),
+
+  // 🐶 Utilise 'varchar' pour créer la colonnes 'title'
+
+  // 🐶 Utilise 'boolean' pour créer la colonnes 'iscompleted'
+
+  // 🐶 Utilise 'timestamp' pour créer les colonnes 'updatedAt' 'createdat'
+
+  // defaultNow() permet de définir la valeur par défaut de la colonne
 })
-export type Todo = typeof todos.$inferSelect // return type when queried
-export type AddTodo = typeof todos.$inferInsert // input type when inserting
+// 🐶 exporte  les type 'Todo' et 'AddTodo'
+
+// 🤖 export type Todo = typeof todos.$inferSelect // return type when queried
+// 🤖 export type AddTodo = typeof todos.$inferInsert // input type when inserting
